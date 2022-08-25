106703b3982552017ded4993eadfb3a33631e952
Denys <heartbreathing19951014@gmail.com>
2022-08-25 21:57:55 +0000

    Added properties for 'loader' class

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,27 @@ window.addEventListener('mousemove', function (e) {
     bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 
 });
+
+// Loading animation for a loading qube.
+
+document.onreadystatechange = function () {
+    // 
+    setTimeout(function () {
+        if (document.readyState !== "complete") {
+            document.querySelector(
+                "body").style.visibility = "hidden";
+            document.querySelector(
+                ".logo").style.visibility = "hidden";
+            document.querySelector(
+                "#loader-wrapper").style.visibility = "visibe";
+        } else {
+            document.querySelector(
+                "#loader-wrapper").style.display = "none";
+            document.querySelector(
+                "body").style.visibility = "visible";
+            document.querySelector(
+                ".logo").style.visibility = "visible";
+        }
+    }, 2000);
+
+};
