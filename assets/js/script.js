efdab8921a5d8ca26b16d276362cfa40890aa45b
Denys <heartbreathing19951014@gmail.com>
2022-08-26 01:22:59 +0000

    Added a function for getting a result of current attemp question

--- assets/js/script.js
+++ assets/js/script.js
@@ -150,3 +150,34 @@ function getNewQuestion() {
     }
     questionCounter++;
 }
+
+/**
+ * Get the result of current attempt question
+ */
+function getResult(element) {
+    const id = parseInt(element.id);
+
+    // get the answer by comparing the id clicked option
+    if (id === currentQuestion.answer) {
+        // set the green color to the correct
+        element.classList.add('correct');
+        // add the indicator to correct mark
+        updateAnswerIndicator('correct');
+        correctAnswers++;
+    } else {
+        // set the red color to the incorrect option
+        element.classList.add('wrong');
+        // set the red color to the icorrect circle
+        updateAnswerIndicator('wrong');
+
+        // if the asnwer is incorrect,show the correct option by adding green color to the correct option
+        const optionLen = optionContainer.children.length;
+        for (let i = 0; i < optionLen; i++) {
+            if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
+                optionContainer.children[i].classList.add('correct');
+            }
+        }
+    }
+    attempt++;
+    unclickbleOptions();
+}
