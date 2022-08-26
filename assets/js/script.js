2daefce9e498caa00c659716f9fbb16e2449e180
Denys <heartbreathing19951014@gmail.com>
2022-08-26 01:16:06 +0000

    Added variables in Js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,20 @@ document.onreadystatechange = function () {
         }
     }, 2000);
 };
+
+// Variables
+
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector(".option-container");
+const answerIndicatorContainer = document.querySelector(".answers-indicator");
+const homeBox = document.querySelector('.home-box');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let avaliableOptions = [];
+let correctAnswers = 0;
+let attempt = 0;
